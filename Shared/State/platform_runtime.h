1c820737ee92c890cce93675207475633c430ca5
BananasAmIRite <jasony1230@gmail.com>
2026-09-03 17:50:23 -0400

    updated final pinout for fc, fixed naming for state transition request func

--- Shared/State/platform_runtime.h
+++ Shared/State/platform_runtime.h
@@ -5,7 +5,7 @@
 
 // platform runtime; defines what APIs are available per platform 
 
-typedef void (*StateTransitionRequestFn)(void *platform, State *next);
+typedef void (*StateTransitionRequestFn)(void *state_machine_ctx, State *next);
 
 // each hosting platform will have to specify: a request_transition func, a state machine context to pass into request_transition, an IO layer, and whether to enable threads
 struct PlatformRuntime {
